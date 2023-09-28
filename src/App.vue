8988ad47d290d62533b12f705f93c8e71a5748de
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-28 17:16:52 +0200

    add stars icon

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-3 my-2 p-3 border" v-for="film in    state.filmList   ">
+      <div class="col-3 my-2 p-3 border" v-for="film in     state.filmList    ">
         <h5>film:</h5>
         <p>titolo originale : {{ film.original_title }}</p>
         <img :src="'https://image.tmdb.org/t/p/w154/' + film.poster_path" alt="">
@@ -21,9 +21,12 @@
           :src="state.baseFlagUrl + (film.original_language === 'en' ? film.original_language = 'gb' : film.original_language).toUpperCase() + '.svg'" />
 
         <p>voto : {{ getVote(film.vote_average) }}</p>
+
+        <i v-for="star in (getVote(film.vote_average))" class="fa fa-star"></i>
+
       </div>
 
-      <div class="col-3 my-2 p-3 border" v-for="serie in    state.tvList   ">
+      <div class="col-3 my-2 p-3 border" v-for=" serie  in     state.tvList    ">
         <h5>serie tv:</h5>
         <p>titolo originale : {{ serie.original_name }}</p>
         <img :src="'https://image.tmdb.org/t/p/w154/' + serie.poster_path" alt="">
@@ -33,9 +36,7 @@
           :src="state.baseFlagUrl + (serie.original_language === 'en' ? serie.original_language = 'gb' : serie.original_language).toUpperCase() + '.svg'" />
 
         <p>voto : {{ getVote(serie.vote_average) }}</p>
-
-        <i class="fa-solid fa-star"></i>
-
+        <i v-for="star in (getVote(serie.vote_average))" class="fa fa-star"></i>
 
       </div>
     </div>
