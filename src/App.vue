b2878842427ef381a63537bee3d6b275b3b074f7
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-28 15:24:04 +0200

    add query for tv series

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
     </div>
     <div class="row">
       <div class="col-3 my-2 p-3 border" v-for="film in    state.filmList   ">
+        <h5>film:</h5>
         <p>titolo originale : {{ film.original_title }}</p>
         <p>titolo : {{ film.title }}</p>
         <p>lingua : {{ film.original_language }}</p>
@@ -19,6 +20,17 @@
           :src="state.baseFlagUrl + (film.original_language === 'en' ? film.original_language = 'gb' : film.original_language).toUpperCase() + '.svg'" />
 
         <p>voto : {{ film.vote_average }}</p>
+      </div>
+
+      <div class="col-3 my-2 p-3 border" v-for="serie in    state.tvList   ">
+        <h5>serie tv:</h5>
+        <p>titolo originale : {{ serie.original_name }}</p>
+        <p>titolo : {{ serie.name }}</p>
+        <p>lingua : {{ serie.original_language }}</p>
+        <img width="100" :alt="serie.original_language"
+          :src="state.baseFlagUrl + (serie.original_language === 'en' ? serie.original_language = 'gb' : serie.original_language).toUpperCase() + '.svg'" />
+
+        <p>voto : {{ serie.vote_average }}</p>
       </div>
     </div>
   </div>
--- src/state.js
+++ src/state.js
@@ -2,25 +2,42 @@ import { reactive } from "vue";
 import axios from '../node_modules/axios'
 
 export const state = reactive({
-    baseURL: 'https://api.themoviedb.org/3/search/movie?api_key=f09ece8e1bf00a0af6a649c3ccfd6bfa',
+    baseFilmURL: 'https://api.themoviedb.org/3/search/movie?api_key=f09ece8e1bf00a0af6a649c3ccfd6bfa',
+    baseSeriesURL: 'https://api.themoviedb.org/3/search/tv?api_key=f09ece8e1bf00a0af6a649c3ccfd6bfa',
     query: '',
     querySpan: '',
     filmList: null,
+    tvList: null,
     baseFlagUrl: 'http://purecatamphetamine.github.io/country-flag-icons/3x2/',
 
 
     fetchData() {
-        axios.get(this.baseURL + `&query=${this.query}`)
+        axios.get(this.baseFilmURL + `&query=${this.query}`)
             .then(response => {
-                console.log('response...', response);
+                //console.log('response...', response);
                 this.querySpan = this.query;
                 this.query = '';
-                console.log(response.data.results);
+                //console.log(response.data.results);
                 this.filmList = response.data.results;
 
             })
             .catch(error => {
                 console.log('error : ', error);
             })
+
+        axios.get(this.baseSeriesURL + `&query=${this.query}`)
+            .then(response => {
+                console.log('response...', response);
+                this.querySpan = this.query;
+                this.query = '';
+                console.log(response.data.results);
+                this.tvList = response.data.results;
+
+            })
+            .catch(error => {
+                console.log('error : ', error);
+            })
     }
+
+
 })
