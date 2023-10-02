41341fc92c9a12873a5ae1a5a0f3b9379024d347
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-02 17:34:02 +0200

    refactoring filters function

--- src/components/AppFilters.vue
+++ src/components/AppFilters.vue
@@ -21,7 +21,8 @@ export default {
 
 <template>
     <ul class="genre list-unstyled m-0 position-fixed" v-show="state.filmList && state.tvList != ''">
-        <li :id="genre" v-for="genre in state.catList" @click="state.filterResults(genre); addClass(genre)"> {{ genre }}
+        <li :id="genre" v-for="genre in state.catList"
+            @click="state.filterResults(genre); addClass(genre); console.log('ciao')"> {{ genre }}
         </li>
     </ul>
 </template>
